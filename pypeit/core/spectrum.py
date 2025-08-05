ab493a51dd25592f927e84217301f38b6d8f6592
Kyle Westfall <westfall@ucolick.org>
2025-08-05 16:56:54 -0700

    spectrum.multiply

--- pypeit/core/spectrum.py
+++ pypeit/core/spectrum.py
@@ -8,6 +8,7 @@ Ideally this would be replaced by specutils.Spectrum
 import numpy as np
 
 from pypeit import msgs
+from pypeit import utils
 
 
 class Spectrum:
@@ -34,21 +35,21 @@ class Spectrum:
         self.wave = np.asarray(wave, dtype=float)
 
         self.flux = np.asarray(flux, dtype=float)
-        if self.flux.size != self.wave.size:
-            msgs.error('Wavelength and flux arrays do not have the same size.')
+        if self.flux.shape != self.wave.shape:
+            msgs.error('Wavelength and flux arrays do not have the same shape.')
 
         if ivar is None:
             self.ivar = None
         else:
             self.ivar = np.asarray(ivar, dtype=float)
-            if self.ivar is not None and self.ivar.size != self.wave.size:
-                msgs.error('Wavelength and inverse variance arrays do not have the same size.')
+            if self.ivar is not None and self.ivar.shape != self.wave.shape:
+                msgs.error('Wavelength and inverse variance arrays do not have the same shape.')
 
         if gpm is None:
-            self.gpm = np.ones(wave.size, dtype=bool)
+            self.gpm = np.ones(self.wave.shape, dtype=bool)
         else:
             self.gpm = np.asarray(gpm, dtype=bool)
-            if self.gpm.size != self.wave.size:
+            if self.gpm.shape != self.wave.shape:
                 msgs.error('Wavelength and good-pixel arrays do not have the same size.')
 
         self.meta = meta
@@ -56,4 +57,42 @@ class Spectrum:
     @property
     def size(self):
         return self.wave.size
+    
+    @property
+    def shape(self):
+        return self.wave.shape
+    
+    def multiply(self, a):
+        """
+        Multiply the spectrum by a scalar or vector.
+
+        This modifies the spectrum in place.  If uncertainties are available,
+        they are propagated; when the multiplicative factor is 0, the inverse
+        variance (if available) is also set to 0.
+
+        Parameters
+        ----------
+        a : scalar, array-like
+            Multiplicative factor.  If an array, its shape must match :attr:`flux`.
+        """
+        if isinstance(a (int, np.integer, float, np.floating)):
+            if a == 0.:
+                msgs.warn('Multiplicative factor is 0!')
+            self.flux *= a
+            if self.ivar is not None:
+                if np.absolute(a) > 0:
+                    self.ivar /= a**2
+                else:
+                    self.ivar *= 0.
+            return
+        
+        _a = np.asarray(a)
+        if _a.shape != self.flux.shape:
+            msgs.error(f'Shape mismatch between spectrum flux array ({self.flux.shape}) and '
+                       f'multiplicative factor array ({_a.shape}).')
+        self.flux *= _a
+        if self.ivar is not None:
+            self.ivar *= utils.inverse(_a**2)
+            self.ivar[np.absolute(_a) == 0.] = 0.
+
 
